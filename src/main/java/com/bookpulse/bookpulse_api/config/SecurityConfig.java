bc12bccba208df4e82f9f89c6fdb6b25c3251a6c
Cristian Alhambra <240956536+cristian-dev92@users.noreply.github.com>
2026-08-21 13:34:52 +0200

    Añadimos la url de vercel a cors para que funcione desplegado

--- src/main/java/com/bookpulse/bookpulse_api/config/SecurityConfig.java
+++ src/main/java/com/bookpulse/bookpulse_api/config/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://bookpulse-frontend-ten.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "X-Requested-With"));
         configuration.setAllowCredentials(true);
